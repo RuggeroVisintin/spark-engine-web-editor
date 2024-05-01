0d680bb662b1d9eec1e2b74c6e26b3115f7676f8
Ruggero Visintin <ruggero.visintin@docebo.com>
2024-05-01 14:59:45 +0200

    chore: update gitingore and add components to project

---
+++ src/layouts/EditorLayout.js
@@ -0,0 +1,12 @@
+import { FlexBox } from "../components";
+
+function EditorLayout () {
+    return (
+        <FlexBox>
+            <h1> Hello World </h1>
+            <div style={{ color: "red" }}> </div>
+        </FlexBox>
+    )
+}
+
+export default EditorLayout;
