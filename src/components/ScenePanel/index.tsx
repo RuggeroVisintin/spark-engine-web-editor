cb99956a27d505275102011e968d9a88f0b0bb58
Ruggero Visintin <ruggero.visintin@docebo.com>
2024-05-07 19:37:37 +0200

    chore: beautify

--- src/components/ScenePanel/index.tsx
+++ src/components/ScenePanel/index.tsx
@@ -9,9 +9,9 @@ interface ScenePanelProps {
 
 export const ScenePanel = ({ entities = [] }: ScenePanelProps) => {
     return (
-        <Box $size={0.25} style={{borderLeft: '2px solid black'}}>
+        <Box $size={0.25} style={{borderLeft: '2px solid black'}} data-testid="ScenePanel">
             <ul>
-                {entities.map((entity) => <li key={uuid()}>{entity.name}</li>)}
+                {entities.map((entity) => <li key={uuid()} data-testid="ScenePanel.EntityEntry">{entity.name}</li>)}
             </ul>
         </Box>
     );
