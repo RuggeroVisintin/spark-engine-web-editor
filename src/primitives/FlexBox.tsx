c346b15ceb6a0f4d1d090843108f54aa3c9d7560
Ruggero Visintin <ruggero.visintin@docebo.com>
2024-05-11 13:34:32 +0200

    refactor: add $fill option to FlexBox

--- src/primitives/FlexBox.tsx
+++ src/primitives/FlexBox.tsx
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 interface FlexBoxProps {
     $direction?: 'column' | 'row';
     $centerItems?: boolean;
+    $fill?: boolean;
 }
 
 export const FlexBox = styled.div<FlexBoxProps>`
     display: flex;
     flex-direction: ${props => props.$direction ?? 'column'};
-    height: 100%;
+    align-items: ${props => props.$centerItems ? 'center' : 'unset'};
 
-    align-items: ${props => props.$centerItems ? 'center' : 'unset'}
+    ${props => props.$fill && `height: 100%;`}
 `
