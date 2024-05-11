753668b9e8b2332bd2b83ea58556997a39ecfbba
Ruggero Visintin <ruggero.visintin@docebo.com>
2024-05-11 13:35:35 +0200

    refactor: add spacing utilities

--- src/primitives/Box.tsx
+++ src/primitives/Box.tsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import { BackgroundColor, TextColor } from "./colors";
+import { Spacing } from "./spacing";
 
 interface BoxProps {
     $size?: number;
+    $spacing?: Spacing
 }
 
 export const Box = styled.div<BoxProps>`
     flex: ${props => props.$size ?? 1};
     color: ${TextColor.Primary};
     background: ${BackgroundColor.Primary};
+
+    ${props => props.$spacing? `padding: ${props.$spacing}`: ''}
 `;
---
+++ src/primitives/spacing.tsx
@@ -0,0 +1,5 @@
+export const enum Spacing {
+    small = '10px',
+    medium = '15px',
+    large = '20px'
+} 
