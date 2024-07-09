d365a533a9365848f7123544e96b12eca910f101
Ruggero Visintin <ruggero.visintin@docebo.com>
2024-07-09 19:13:25 +0200

    refactor: action menu WIP

---
+++ src/components/PopupMenu/PopupMenu.test.tsx
@@ -0,0 +1,41 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { PopupMenu } from ".";
+
+describe('PopupMenu', () => {
+    it('Should render the menu label based on the given config', () => {
+        const popupMenu = <PopupMenu data-testid='test-popup-menu' label="File"></PopupMenu>;
+
+        render(popupMenu);
+        expect(screen.getByTestId(`test-popup-menu.trigger`)).toHaveTextContent('File');
+    })
+    
+    it.todo('Should invoke the given callback when item is clicked if action is defined');
+    
+    it('Should open without any child menu at screen', () => {
+        const popupMenu = <PopupMenu data-testid='test-popup-menu-file' label="File" items={[{
+            label: 'Open'
+        }, {
+            label: 'Save'
+        }]}></PopupMenu>;
+
+        render(popupMenu);
+
+        expect(screen.queryAllByTestId(`test-popup-menu-file.item`)).toEqual([]);
+    })
+    it('Should open the child submenu when children property is defined', () => {
+        const popupMenu = <PopupMenu data-testid='test-popup-menu-file' label="File" items={[{
+            label: 'Open'
+        }, {
+            label: 'Save'
+        }]}></PopupMenu>;
+
+        render(popupMenu);
+        fireEvent.click(screen.getByTestId(`test-popup-menu-file.trigger`));
+
+        const items = screen.queryAllByTestId(`test-popup-menu-file.item`);
+
+        expect(items[0]).toHaveTextContent('Open');
+        expect(items[1]).toHaveTextContent('Save');
+    });
+})
--- src/templates/ActionMenu/index.tsx
+++ src/templates/ActionMenu/index.tsx
@@ -1,23 +1,18 @@
 
-import React, { useState } from "react";
-import styled from "styled-components";
-import { ListItem } from "../../components";
-import { PopupMenu } from "../../patterns/PopupMenu";
+import React from "react";
+import { PopupMenu } from "../../components/PopupMenu";
 import { BackgroundColor, FlexBox, TextColor } from "../../primitives";
 
-const MenuItem = styled(ListItem)`
-    display: block;
-`
+
 
 export const ActionMenu = () => {
-    const [fileMenuOpened, setFileMenuOpened] = useState(false);
-
-
-    
     return (
         <FlexBox style={{ height: '40px', background: BackgroundColor.Primary, borderBottom: `1px solid ${TextColor.Primary}` }} $direction='row'>
-            <MenuItem text="File" flex={false} onClick={() => setFileMenuOpened(true)} />
-            { fileMenuOpened && <PopupMenu></PopupMenu> }
+            <PopupMenu label="File" items={[{
+                label: 'Open'
+            }, {
+                label: 'Save'
+            }]}></PopupMenu>
         </FlexBox>
     )
 }
