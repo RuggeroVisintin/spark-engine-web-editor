20dc915b45456ddd5f2012524ba3d0cb29c9111f
Ruggero Visintin <ruggero.visintin@docebo.com>
2024-08-28 20:09:44 +0200

    refactor(core): add SaveSceneUseCase

---
+++ src/core/scene/usecases/SaveSceneUseCase.tsx
@@ -0,0 +1,10 @@
+import { Scene } from "sparkengineweb";
+import { SceneRepository } from "../ports";
+
+export class SaveSceneUseCase {
+    constructor(private readonly sceneRepo: SceneRepository) { }
+
+    public async execute(scene: Scene): Promise<void> {
+        await this.sceneRepo.save(scene.toJson());
+    }
+}
