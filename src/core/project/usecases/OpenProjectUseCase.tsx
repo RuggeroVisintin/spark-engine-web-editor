d90043245f527acf003085e2694ea29b74a74dfc
Ruggero Visintin <ruggero.visintin@docebo.com>
2024-09-21 01:40:54 +0200

    refactor(project): working on OpenProjectUseCase

---
+++ src/core/project/usecases/OpenProjectUseCase.tsx
@@ -0,0 +1,6 @@
+export class OpenProjectUseCase {
+    public async execute(): Promise<void> {
+        // TODO - use fs api to prompt the user to select the project directory
+        // once the directory is loaded load 
+    }
+}
