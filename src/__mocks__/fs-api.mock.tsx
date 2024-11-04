e22f173a2c6fb226b92115545d7f84255caf5151
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2024-11-04 23:16:43 +0100

    test(mocks): improve web fs-api directoryHandleMock

--- src/__mocks__/fs-api.mock.tsx
+++ src/__mocks__/fs-api.mock.tsx
@@ -10,10 +10,16 @@ export const FileSystemWritableFileStreamMock = {
 }
 
 
+
 export const createDirectoryHandleMock = () => ({
     kind: 'directory',
     name: 'test-dir',
-    getFileHandle: jest.fn().mockResolvedValue({ kind: 'file', name: 'open-test', getFile: jest.fn().mockResolvedValue({ text: jest.fn(() => Promise.resolve(__mockedFileBody)) }) }),
+    getFileHandle: jest.fn().mockResolvedValue({
+        kind: 'file',
+        name: 'open-test',
+        getFile: jest.fn().mockResolvedValue({ text: jest.fn(() => Promise.resolve(__mockedFileBody)) }),
+        createWritable: jest.fn().mockResolvedValue(FileSystemWritableFileStreamMock)
+    }),
     getDirectoryHandle: () => createDirectoryHandleMock()
 });
 
