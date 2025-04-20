2613f3ef62df801bc8b3252fc6efbd5355d2d189
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2025-04-20 13:12:12 +0200

    refactor: add IDebuggerEntity interface

--- src/core/debug/EntityOtuline.tsx
+++ src/core/debug/EntityOtuline.tsx
@@ -1,9 +1,12 @@
-import { GameObject, Rgb } from "sparkengineweb";
+import { GameObject, IEntity, Rgb, TransformComponent } from "sparkengineweb";
+import IDebuggerEntity from "./IDebuggerEntity";
 
-export class EntityOutline extends GameObject {
+let UNIQUE_COUNTER = 0;
+
+export class EntityOutline extends GameObject implements IDebuggerEntity {
     constructor() {
         super({
-            name: 'EntityOutline',
+            name: UNIQUE_COUNTER === 0 ? 'EntityOutline' : `EntityOutline${UNIQUE_COUNTER}`,
             material: {
                 diffuseColor: new Rgb(255, 255, 0)
             },
@@ -14,5 +17,19 @@ export class EntityOutline extends GameObject {
                 depthIndex: 0
             }
         });
+
+        UNIQUE_COUNTER++;
+    }
+
+    match(target: IEntity): void {
+        const debuggerTransform = this.getComponent<TransformComponent>('TransformComponent');
+        const targetTransform = target.getComponent<TransformComponent>('TransformComponent');
+
+        if (!targetTransform || !debuggerTransform) {
+            return;
+        }
+
+        debuggerTransform.position = targetTransform.position;
+        debuggerTransform.size = targetTransform.size;
     }
 }
