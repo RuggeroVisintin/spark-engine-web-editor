70bc7f7174c88efa4df217103c7efb26e76b6fdb
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2025-07-24 20:46:03 +0200

    refactor: fix export

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,5 @@
 export * from './useAppState';
 export * from './withMemoryRouter';
-export * from './useOnInit';
 export * from './withStrictMode';
 export * from './useScriptEditorService';
+export * from './useEditorService';
